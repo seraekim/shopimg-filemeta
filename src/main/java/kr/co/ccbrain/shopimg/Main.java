6227a264aca95881b30b7001d214f76e254c462d
srkim <srkim@DESKTOP-SLKS72Q>
2017-04-20 20:42:49 +0900

    log

--- src/main/java/kr/co/ccbrain/shopimg/Main.java
+++ src/main/java/kr/co/ccbrain/shopimg/Main.java
@@ -1,5 +1,7 @@
 package kr.co.ccbrain.shopimg;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,17 +9,19 @@ import kr.co.ccbrain.shopimg.service.MainService;
 
 @Component
 public class Main {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Autowired
 	MainService mainService;
+
 	public void init() {
-		System.out.println("init");
+		logger.info("init");
 		try {
-			System.out.println(mainService.init());;
+			System.out.println(mainService.init());
+			;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }
